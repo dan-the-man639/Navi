dd381401bcaf97d8c307153eac373d1a8b5654f6
James Liang <86000097+jamesliangg@users.noreply.github.com>
2025-09-13 20:22:08 -0400

    Update layout.tsx

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'dataFind',
+  description: 'Created with love',
   generator: 'v0.app',
 }
 
